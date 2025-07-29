3a5ed4ffbc97e383aa017eef4caeb6f5c2b5c116
jobaydulcse <jobaydul.cse@gmail.com>
2025-07-29 21:02:12 +0600

    for-of added

---
+++ for-of.js
@@ -0,0 +1,21 @@
+const number = [
+  11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+  30,
+];
+console.log(number);
+for (const num of number) {
+  console.log(num);
+}
+
+// Basic structure
+// for(){
+
+// }
+
+const fruits = ["Orange", "Apple", "Banana", "Jackfruit", "Watermelon"];
+
+for (const fruit of fruits) {
+  console.log("I want to eat");
+
+  console.log(fruit);
+}
