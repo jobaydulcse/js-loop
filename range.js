70cb40c5d9d32577e371c7e873a3485a6a6ecff4
jobaydulcse <jobaydul.cse@gmail.com>
2025-07-30 09:56:03 +0600

    while fucntion added

---
+++ range.js
@@ -0,0 +1,10 @@
+// let num = 1;
+// while (num <= 10) {
+//   console.log(num);
+//   num++;
+// }
+let num = 100;
+while (num <= 300) {
+  console.log(num);
+  num++;
+}
---
+++ while.js
@@ -0,0 +1,20 @@
+const number = 7;
+if (number < 10) {
+  console.log("Coto ekta number paiche");
+}
+console.log("Something is cooking");
+/*
+ ********while loop******
+ */
+// const num = 0;
+// while (num < 5) {
+//   console.log("looping", num);
+// }
+
+let num = 0;
+while (num < 5) {
+  console.log("looping", num);
+  //   num = num + 1;
+  //   num += 1;
+  num++;
+}
